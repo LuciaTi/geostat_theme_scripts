bdd28a9ea202ac41a6a140f8b71727ad7a9bed94
LuciaTi <lucia.tischer@gmx.de>
2017-12-10 16:16:08 +0100

    Some addition about plotting (ggR).

--- Theme_LandCoverClassification.R
+++ Theme_LandCoverClassification.R
@@ -6,6 +6,7 @@ setwd("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geos
 
 library(RStoolbox)
 library(raster)
+library(ggplot2)
 
 lsat <- brick("lsat.tif")
 
@@ -14,19 +15,20 @@ lsat <- brick("lsat.tif")
 uc <- unsuperClass(lsat, nClasses = 3)
 plot(uc$map)
 
+cols <- c("1"="darkgreen", "2"="blue", "3"="darkred")
 ggR(uc$map, forceCat = TRUE, geom_raster = TRUE) +
   ggtitle("Unsupervised classification 1") +
-  theme(plot.title = element_text(size = 12, colour = "black", face="bold"))
-
-# + scale_colour_manual(values = c("white", "blue", "red"),  ## didn´t work yet...
-#                      labels = c("water", "urban", "forest"))
+  theme(plot.title = element_text(size = 12, colour = "black", face="bold")) +
+  scale_fill_manual(values = cols, 
+                    labels=c("Class1: Forest", "Class2: Water", "Class3: non-Forest"), 
+                    name = "Classes\n")
 
 
 ## 2) Unsupervised L.C.Classification - Second Possibility ####
 # run an unsupervised classification 2
 plotRGB(lsat, stretch = "lin")
 ext <- drawExtent()
-lsat_ext <- crop(lsat, ext)
+lsat_ext <- crop(lsat, ext) # kmeans only for small rasters --> cropping can be usefull
 
 lsat_ext.kmeans <- kmeans(lsat_ext[], 3) # read in as matrix (kmeans can´t read raster)
 lsat.kmeans <- kmeans(lsat[], 3)
@@ -35,14 +37,19 @@ kmeansraster.big <- raster(lsat)
 kmeansraster[] <- lsat_ext.kmeans$cluster # fill up the empty raster with values
 kmeansraster.big[] <- lsat.kmeans$cluster
 
-plot(kmeansraster)
+plot(kmeansraster) # first possibility for plotting 
 plot(kmeansraster.big)
 
-ggR(kmeansraster.big, forceCat = TRUE, geom_raster = TRUE) +
-  ggtitle("Unsupervised classification 2") +
-  theme(plot.title = element_text(size = 12, colour = "black", face="bold"))
 
-plotRGB(lsat, stretch = "lin")
+cols <- c("1"="darkgreen", "2"="darkred", "3"="blue") # second possiblity for plotting
+ggR(kmeansraster.big, forceCat = TRUE, geom_raster = TRUE) + 
+  ggtitle("Unsupervised classification 2") +
+  scale_fill_manual(values = cols, 
+                    labels=c("Class1: Forest", "Class2: non-Forest", "Class3: Water"), 
+                    name = "Classes\n")
+
+  
+plotRGB(lsat, stretch = "lin") # third possibility for plotting
 plot(kmeansraster.big) # plot with pre-defined colours
 click(kmeansraster.big, n = 3)
 arg <- list(at = seq(1,3,1), labels = c("forest", "water", "non-forest"))
