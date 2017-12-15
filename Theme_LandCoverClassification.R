53557b4041a71e594c0f99383e2389119e442fd1
LuciaTi <lucia.tischer@gmx.de>
2017-12-15 12:22:19 +0100

    Addition: loop to caluclate several sc at once and plot them

--- Theme_LandCoverClassification.R
+++ Theme_LandCoverClassification.R
@@ -10,8 +10,8 @@ library(ggplot2)
 library(rgdal)
 library(wrspathrow)
 
-lsat <- brick("lsat.tif")
-p224r63 <- brick("crop_p224r63_all_bands.tif")
+lsat <- brick("raster/lsat.tif")
+p224r63 <- brick("raster/crop_p224r63_all_bands.tif")
 
 
 ## 1) Unsupervised L.C.Classification - unsuperClass() ####
@@ -29,7 +29,7 @@ ggR(uc$map, forceCat = TRUE, geom_raster = TRUE) +
 
 ## 2A) Unsupervised L.C.Classification - kmeans() and correction for NAs ####
 plotRGB(lsat, stretch = "lin") # plot the original image
-lsat.kmeans <- kmeans(lsat[], 3) # read in as matrix (kmeans can´t read raster)
+lsat.kmeans <- kmeans(lsat[], 3) # read in as matrix (kmeans can?t read raster)
 kmeansraster <- raster(lsat) # re-create a raster from the result
 kmeansraster[] <- lsat.kmeans$cluster # fill up the empty raster with value
 
@@ -98,13 +98,13 @@ plot2
 
 
 
-## 3) Supervised Landcover Classification - randomForest (model="rf) ####
+## 3A) Supervised Landcover Classification - randomForest (model="rf) ####
 
-lsat <- brick("lsat.tif") # load the data
-p224r63 <- brick("crop_p224r63_all_bands.tif")
+lsat <- brick("raster/lsat.tif") # load the data
+p224r63 <- brick("raster/crop_p224r63_all_bands.tif")
 
 # read in the training data
-td <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts",
+td <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts/vector",
               "classification_1_data")
 # run the classification
 sc <- superClass(p224r63, 
@@ -123,16 +123,18 @@ ggR(sc$map, forceCat = TRUE, geom_raster = TRUE) +
                     labels=c("Class1: Forest", "Class2: Water", "Class3: non-Forest"), 
                     name = "Classes\n")
 
-writeRaster(sc$map, filename="sc_classification_1.tif") # safe the map to put it in QGIS for example.
+writeRaster(sc$map, filename="results/sc_p224r63_1.tif") # safe the map to put it in QGIS for example.
 
 
-
+## 3B) Supervised Landcover Classification - randomForest ("rf") but with class streets ####
 
 # same classification, but additionally with class "streets"
-td_2 <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts",
+td_2 <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts/vector",
               "classification_1_data_2")
 # run the classification
-sc_2 <- superClass(p224r63, trainData=td_2, responseCol="id")
+sc_2 <- superClass(p224r63, 
+                   trainData=td_2, 
+                   responseCol="id")
 
 # plot the classification
 plot(sc_2$map)
@@ -145,27 +147,60 @@ ggR(sc_2$map, forceCat = TRUE, geom_raster = TRUE) +
                     labels=c("Class1: Water", "Class2: Forest", "Class3: non-Forest", "Class4: Streets"), 
                     name = "Classes\n")
 
+writeRaster(sc_2$map, filename="results/sc_p224r63_2.tif") # safe the map
 
-## 4) supervised Classification - 
 
-# read in the image to classify
-p224r63 <- brick("crop_p224r63_all_bands.tif")
+## 4) supervised Classification - models "rf","smvRadial","pls" and Entropy ####
 
 # read in the training data
-td <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts",
+td <- readOGR("C:/Users/Ltischer/Documents/Studium/A Master/Geostatistics/R-Skripte/geostat_theme_scripts/vector",
               "classification_1_data")
+
 # run the classification
-sc <- superClass(p224r63, trainData=td, responseCol="id", model=)
+#sc <- superClass(p224r63, trainData=td, responseCol="id", model=)
+
+model_names <- c("rf", "svmRadial", "pls") # define witch models shell be used
+
+for (i in 1:length(model_names)){ # for the whole vector of models (model[1] to last model) ...
+  sc_3 <- superClass(p224r63, 
+                   trainData=td, 
+                   responseCol = "id",
+                   model=model_names[i]) # ... run the classification like so and use model[i]]
+  
+  names(sc_3$map) <- model_names[i] # directly rename the name of the layers (new name == name of used model)
+  
+  # plot each classifcation
+  p <- ggR(sc_3$map, forceCat = TRUE, geom_raster = TRUE) +
+    ggtitle(paste("Supervised classification - model:", model_names[i])) +
+    theme(plot.title = element_text(size = 12, colour = "black", face="bold")) +
+    scale_fill_manual(values = cols, 
+                      labels=c("Class1: Water", "Class2: Forest", "Class3: non-Forest"), 
+                      name = "Classes\n")
+  print(p)
+  
+  if (i==1){                     # in the first run: safe the sc-result as sc_stack
+            sc_3_stack <- sc_3$map
+           } else{                        # later on: stack the actual sc-result with sc_stack 
+                  sc_3_stack <- stack(sc_3_stack, sc_3$map)
+                 }
+}
+
 
 # plot the classification
-plot(sc$map)
 
 cols <- c("1"="blue", "2"="darkgreen", "3"="darkred")
-ggR(sc$map, forceCat = TRUE, geom_raster = TRUE) +
-  ggtitle("Supervised classification 1") +
+ggR(sc_3$map["pls"], forceCat = TRUE, geom_raster = TRUE) +
+  ggtitle("Supervised classification - model pls") +
   theme(plot.title = element_text(size = 12, colour = "black", face="bold")) +
   scale_fill_manual(values = cols, 
                     labels=c("Class1: Forest", "Class2: Water", "Class3: non-Forest"), 
+                    name = "Classes\n")
+
+ggR(test, forceCat = TRUE, geom_raster = TRUE) +
+  ggtitle(paste("Supervised classification - model:", model_names[3])) +
+  theme(plot.title = element_text(size = 12, colour = "black", face="bold")) +
+  scale_fill_manual(values = cols, 
+                    labels=c("Class1: Water", "Class2: Forest", "Class3: non-Forest"), 
                     name = "Classes\n")
 
 
